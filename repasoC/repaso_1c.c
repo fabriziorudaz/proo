5ba4c0ad78cd6716693c8c587203c9ec287c1617
fabriziorudaz <130599313+fabriziorudaz@users.noreply.github.com>
2024-03-27 11:40:14 -0300

    Create repaso_1c.c

---
+++ repasoC/repaso_1c.c
@@ -0,0 +1,21 @@
+int main (){
+    float cp1, cp2, cp3, ef, tf;
+
+     printf("ingrese calificacion parcial 1 ");
+     scanf("%d", &cp1);
+      printf("ingrese calificacion parcial 2 ");
+     scanf("%d", &cp2);
+     printf("ingrese calificacion parcial 2 ");
+     scanf("%d", &cp3);
+      printf("ingrese examen final");
+     scanf("%d", &ef);
+      printf("ingrese trabajo final ");
+     scanf("%d", &tf);
+
+     float calificacion = 0.55 * ((cp1+cp2+cp3)/3) + 0.30 * ef + 0.15 * tf;
+
+     printf("la calificacion final del alumno fue %.2f", calificacion);
+
+     return 0;
+     
+}
