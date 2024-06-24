e3cfa555a806591e72f290eeed03f9512bea36a1
fabriziorudaz <130599313+fabriziorudaz@users.noreply.github.com>
2024-06-24 15:16:14 -0300

    Update prueba.c

--- prueba.c
+++ prueba.c
@@ -12,7 +12,7 @@ void setColor(int textColor, int bgColor) {
 
 //aqui voy a  poner una funcion la cual me almacene la cantidad de alfajores 
 
-void alfajores (char * alfaj[], int cantidades[], int cajas){
+ void alfajores (char * alfaj[], int cantidades[], int cajas){
 
  srand(time(NULL));
  //estos son los tipos de alfajopres que tengo
@@ -24,6 +24,7 @@ void alfajores (char * alfaj[], int cantidades[], int cajas){
             int cantidad = rand() % 101; // Genera una cantidad aleatoria entre 0 y 100
             cantidades[j] += cantidad;
 			  // Suma la cantidad de alfajores al tipo correspondiente
+			  setColor(15, 200);
             printf("%s: %d\n", tipos_alfaj[j], cantidad);
         } 
         printf("\n");
@@ -48,19 +49,23 @@ void ganancia_error(int produccion_empleado[][3], int fuera_produccion) {
 }
 }
 
-//operarios son 6 y arman las cajas 
-
 int main() {
 	srand(time(NULL));
     char *tipos_alfaj[3] = {"chocolate", "ddl", "fruta"};
     int produccion_empleado[6][3] = {0};
-    int fuera_de_produccion = rand () % 3 ;
-    int cajas; // Ejemplo de 5 cajas
+    int fuera_produccion = rand () % 3 ;
+    int cajas;
     puts ("ingrese la cantidad de cajas que desee");
-    scanf("%d", &cajas);
+    scanf("%d ", &cajas);
+    
     // Llamada a la función para simular la producción de cajas de alfajores
-    alfajores(tipos_alfaj, produccion_empleado, cajas);
+    alfajores (tipos_alfaj, produccion_empleado, cajas);
 
+    // Llamada a la función para mostrar la producción por empleado y tipo de alfajor
+    ganancia_error(produccion_empleado, fuera_produccion);
+
+    return 0;
+}
     // Llamada a la función para mostrar la producción por empleado y tipo de alfajor
     ganancia_error(produccion_empleado, fuera_de_produccion);
 
