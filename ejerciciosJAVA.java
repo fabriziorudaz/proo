9106632f58bcb738d4f97c7e5b6c2d9315d83f33
fabriziorudaz <130599313+fabriziorudaz@users.noreply.github.com>
2024-08-12 17:11:08 -0300

    Create ejerciciosJAVA.java

---
+++ ejerciciosJAVA.java
@@ -0,0 +1,16 @@
+package ejerciciosPoo;
+
+public class numeros {
+	
+	public static void main(String args[]) {
+		for(int i = 0; i <= 100; i++) {
+			if(i % 2 == 0) {
+			System.out.println(i +" es par"); 
+		}else {
+			System.out.println(i +" es impar");
+		}
+			}
+		System.out.println("HOLA MUNDO");
+	}
+
+}
