b38351f4e372c6fcd149dc1265029d8e5c7cf065
fabriziorudaz <130599313+fabriziorudaz@users.noreply.github.com>
2024-09-16 17:19:02 -0300

    Create 6JAVA.java

---
+++ ejercicio-java/6JAVA.java
@@ -0,0 +1,16 @@
+public class eje6 {
+public static void main(String[] args) {
+	
+	int num1=10;
+	int num2=5;
+
+	System.out.println("El resultado de la suma es " +(num1+num2));
+
+	System.out.println("El resultado de la resta es " +(num1-num2));
+
+	System.out.println("El resultado de la multiplicacion es " +(num1*num2));
+
+	System.out.println("El resultado de la division es " +(num1/num2));
+
+}
+}
