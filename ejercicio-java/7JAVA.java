f1ec58ac7aabed925d7820244832cd5a9043142b
fabriziorudaz <130599313+fabriziorudaz@users.noreply.github.com>
2024-10-02 12:14:27 -0300

    Create 7JAVA.java

---
+++ ejercicio-java/7JAVA.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ejercicio7 {
+	 public static void main(String[] args) {
+	        
+	       
+	        Scanner sc = new Scanner(System.in);
+	        
+	        System.out.println("Introduce tu nombre: ");
+	        
+	        String nombre=sc.nextLine();
+	 
+	        System.out.println("Bienvenido a la locura "+nombre);
+	    }
+	    
+}
